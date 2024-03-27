c46726e5ae4596124a7204bf008173993b9900fc
praveen <praveenravi4898@gmail.com>
2024-03-27 17:42:19 +0530

    useradded

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-app.use("/",userRouter)
+app.use("/user",userRouter)
 app.use("/employees",empRouter)
 
 app.use("/",(req,res)=>{
